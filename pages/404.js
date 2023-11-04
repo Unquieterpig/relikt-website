30406bda1ca15c2ee37b82c2b8d86b7d6ca3fd85
unquieterpig <unquieterpig@gmail.com>
2023-11-03 20:09:07 -0400

    Added navbar and new pages

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,16 @@
 import Link from 'next/link';
 
+import Navbar from '@components/NavBarHome';
+
 export default function Custom404() {
     return (
         <main>
+            <Navbar />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
             <h1>404 - Page not found</h1>
             <iframe
                 src="https://giphy.com/embed/H7wajFPnZGdRWaQeu0"
