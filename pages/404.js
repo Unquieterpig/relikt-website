754ae2cd0a74fd882e07da02a269c923e7a3a5c2
unquieterpig <unquieterpig@gmail.com>
2023-11-07 23:17:02 -0500

    centered 404 page

--- pages/404.js
+++ pages/404.js
@@ -4,24 +4,26 @@ import Navbar from '@components/NavBarHome';
 
 export default function Custom404() {
     return (
-        <>
-            <Navbar />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <h1>404 - Page not found</h1>
-            <iframe
-                src="https://giphy.com/embed/H7wajFPnZGdRWaQeu0"
-                width="480"
-                height="362"
-                frameBorder="0"
-                allowFullScreen
-                ></iframe>
-                <Link href="/">
-                    <button className='btn-blue'>Go home</button>
-                </Link>
-        </>
+      <>
+        <Navbar />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <center>
+          <h1>404 - Page not found</h1>
+          <iframe
+            src="https://giphy.com/embed/H7wajFPnZGdRWaQeu0"
+            width="480"
+            height="362"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+          <Link href="/">
+            <button className="btn-blue">Go home</button>
+          </Link>
+        </center>
+      </>
     );
 }
