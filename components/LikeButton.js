4d69838c85e32a05eeeb7b4b07aa8ced8e686f7b
Unquieterpig <unquieterpig@gmail.com>
2023-11-09 15:05:04 -0500

    Refactor LikeButton component to use Firebase writeBatch

--- components/LikeButton.js
+++ components/LikeButton.js
@@ -1,18 +1,17 @@
-import { firestore, auth, increment } from '@lib/firebase';
+import { firestore, auth } from '@lib/firebase';
+import { collection, doc, increment, writeBatch } from 'firebase/firestore';
 import { useDocument } from 'react-firebase-hooks/firestore';
-
-// todo; fix this.  it's not working
 
 // Allows user to like a post 
 export default function Like({ postRef }) {
   // Listen to like document for currently logged in user
-  const likeRef = postRef.collection('likes').doc(auth.currentUser.uid);
+  const likeRef = doc(collection(postRef, 'likes'), auth.currentUser.uid);
   const [likeDoc] = useDocument(likeRef);
 
   // Create a user-to-post relationship
   const addLike = async () => {
     const uid = auth.currentUser.uid;
-    const batch = firestore.batch();
+    const batch = writeBatch(firestore);
 
     batch.update(postRef, { likeCount: increment(1) });
     batch.set(likeRef, { uid });
@@ -22,7 +21,7 @@ export default function Like({ postRef }) {
 
   // Remove a user-to-post relationship
   const removeLike = async () => {
-    const batch = firestore.batch();
+    const batch = writeBatch(firestore);
 
     batch.update(postRef, { likeCount: increment(-1) });
     batch.delete(likeRef);
