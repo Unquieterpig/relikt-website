4507ee198bd55e132bfe8f999d710f14eeb97a29
Unquieterpig <unquieterpig@gmail.com>
2023-11-09 19:05:02 -0500

    Add Next UI, Framer Motion, and dark mode support

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,17 @@ import { UserContext } from '@lib/context';
 import { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useUserData } from '@lib/hooks';
+import { NextUIProvider } from '@nextui-org/react';
 
 function MyApp({ Component, pageProps }) {
   const userData = useUserData();
 
   return (
-    <UserContext.Provider value={ userData }>
-      <Component {...pageProps} />
-      <Toaster />
+    <UserContext.Provider value={userData}>
+      <NextUIProvider>
+        <Component {...pageProps} />
+        <Toaster />
+      </NextUIProvider>
     </UserContext.Provider>
   );
 }
