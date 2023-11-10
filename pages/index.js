e1dfa0c67c10e35e21377716c3b5e0e378590ed1
unquieterpig <unquieterpig@gmail.com>
2023-11-09 23:44:52 -0500

    Refactor homepage layout

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,6 @@ export default function Home(){
   return (
     <>
       <Navbar />
-      <br />
-      <br />
-      <br />
       <HomePage />
     </>
   );
@@ -24,6 +21,39 @@ export default function Home(){
 function HomePage() {
 
   return (
-    <h1 className='antialiased font-sans text-3x1'> Work in Progress</h1>
+    <div className="bg-relikt snap-y snap-mandatory overflow-y-scroll h-screen">
+      <Section1 />
+      <Section2 />
+      <Section3 />
+    </div>
+  );
+}
+
+function Section1() {
+  return (
+    <div className="snap-start">
+      <div className="flex flex-col justify-center items-center h-screen">
+        <h1 className="text-6xl font-bold text-white">RELIKT</h1>
+        <p className="text-2xl text-white">The best way to create a website.</p>
+      </div>
+    </div>
+  );
+}
+
+function Section2() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen snap-start">
+      <h1 className="text-6xl font-bold text-white">RELIKT</h1>
+      <p className="text-2xl text-white">The best way to create a website.</p>
+    </div>
+  );
+}
+
+function Section3() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen snap-start">
+      <h1 className="text-6xl font-bold text-white">RELIKT</h1>
+      <p className="text-2xl text-white">The best way to create a website.</p>
+    </div>
   );
 }
