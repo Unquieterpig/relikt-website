00bb34e93915e6dea277c60184afb1b79f96f5f8
unquieterpig <unquieterpig@gmail.com>
2023-11-10 23:03:32 -0500

    Remove unused code and files.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 import Loader from '@components/Loader'
 import Navbar from '@components/NavBar'
-
-import toast from 'react-hot-toast'
 
 import { UserContext } from '@lib/context'
 
