4e038425bec3804f62f60aea6eaf8b3844140b57
unquieterpig <unquieterpig@gmail.com>
2023-11-11 02:29:31 -0500

    Update Section4 layout in index.js

--- pages/index.js
+++ pages/index.js
@@ -208,12 +208,12 @@ function Section3() {
 
 function Section4() {
   return (
-    <div className="h-screen py-24 sm:py-32 flex justify-center items-center">
+    <div className="h-screen py-24 sm:py-32">
       <div className="mx-auto px-6 lg:px-8">
         <h2 className="text-center text-lg font-semibold leading-8 text-gray-900 dark:text-gray-300">
           Trusted by the world’s most innovative man
         </h2>
-        <div className="mx-auto mt-10 grid grid-cols-2 items-center gap-x-8 gap-y-10 max-w-7xl sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:grid-cols-5">
+        <div className="mx-auto mt-10 grid grid-cols-2 items-center gap-x-8 gap-y-10 max-w-7xl sm:grid-cols-4 sm:gap-x-10 lg:mx-0 lg:grid-cols-5">
           <Image
             as={NextImage}
             className="opacity-100 col-span-2 max-h-full w-full object-contain lg:col-span-1"
