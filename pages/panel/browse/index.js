70950fc36f7132e7f5db89144518022c4c3c8887
Unquieterpig <unquieterpig@gmail.com>
2023-11-14 11:20:13 -0500

    Updated post limit Added meta tags

--- pages/panel/browse/index.js
+++ pages/panel/browse/index.js
@@ -1,79 +1,95 @@
-import PostFeed from '@components/PostFeed';
-import Loader from '@components/Loader';
+import PostFeed from "@components/PostFeed";
+import Loader from "@components/Loader";
+import Metatags from "@components/Metatags";
 
-import Link from 'next/link';
+import Link from "next/link";
 
-import { firestore, postToJSON } from '@lib/firebase';
-import { Timestamp, collectionGroup, getDocs, limit, orderBy, query, startAfter, where } from 'firebase/firestore';
+import { firestore, postToJSON } from "@lib/firebase";
+import {
+  Timestamp,
+  collectionGroup,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+  where,
+} from "firebase/firestore";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 // Max post to query per page
-const LIMIT = 2;
+const LIMIT = 10;
 
 export async function getServerSideProps(context) {
-    const postsQuery = query(
-        collectionGroup(firestore, 'posts'),
-        where('published', '==', true),
-        orderBy('createdAt', 'desc'),
-        limit(LIMIT)
-    );
+  const postsQuery = query(
+    collectionGroup(firestore, "posts"),
+    where("published", "==", true),
+    orderBy("createdAt", "desc"),
+    limit(LIMIT)
+  );
 
-    const posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+  const posts = (await getDocs(postsQuery)).docs.map(postToJSON);
 
-    return {
-        props: { posts }, // will be passed to the page component as props
-    
-    };
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
 }
 
+export default function Browse(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
 
-export default function Browse( props ){
-    const [posts, setPosts] = useState(props.posts);
-    const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
 
-    const [postsEnd, setPostsEnd] = useState(false);
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
 
-    const getMorePosts = async () => {
-        setLoading(true);
-        const last = posts[posts.length - 1];
+    const cursor =
+      typeof last.createdAt === "number"
+        ? Timestamp.fromMillis(last.createdAt)
+        : last.createdAt;
 
-        const cursor = typeof last.createdAt === 'number' ? Timestamp.fromMillis(last.createdAt) : last.createdAt;
+    const postsQuery = query(
+      collectionGroup(firestore, "posts"),
+      where("published", "==", true),
+      orderBy("createdAt", "desc"),
+      startAfter(cursor),
+      limit(LIMIT)
+    );
 
-        const postsQuery = query(
-            collectionGroup(firestore, 'posts'),
-            where('published', '==', true),
-            orderBy('createdAt', 'desc'),
-            startAfter(cursor),
-            limit(LIMIT)
-        );
+    const newPosts = (await getDocs(postsQuery)).docs.map((doc) => doc.data());
 
-        const newPosts = (await getDocs(postsQuery)).docs.map((doc) => doc.data());
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
 
-        setPosts(posts.concat(newPosts));
-        setLoading(false);
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
 
-        if (newPosts.length < LIMIT) {
-            setPostsEnd(true);
-        }
-    };
+  return (
+    <main>
+      <Metatags
+        title="Relikt - Secure Your Voice"
+        description="Homepage of website"
+      />
+      <div className="card">
+        <Link href="/panel/browse/admin">
+          <button className="btn-blue">Create New Post</button>
+        </Link>
+      </div>
+      <PostFeed posts={posts} />
 
-    return (
-        <>
-            <main>
-            <div className='card'> 
-            <Link href="/panel/browse/admin">
-                <button className='btn-blue'>Create New Post</button> 
-            </Link>
-            </div>
-            <PostFeed posts={posts} />
+      {posts.length === 0
+        ? "No posts to load"
+        : !loading &&
+          !postsEnd && <button onClick={getMorePosts}>Load more</button>}
 
-            {posts.length === 0 ? ('No posts to load') : (!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>)}
+      <Loader show={loading} />
 
-            <Loader show={loading} />
-
-            {postsEnd && 'You have reached the end!'}
-        </main>
-        </>
-    )
+      {postsEnd && "You have reached the end!"}
+    </main>
+  );
 }
