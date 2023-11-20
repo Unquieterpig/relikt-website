f9ece730e126a2a267b1aec416a0d6f841c0790f
unquieterpig <unquieterpig@gmail.com>
2023-11-20 00:18:18 -0500

    Refactor ImageUploader component and add Button component

--- components/ImageUploader.js
+++ components/ImageUploader.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
-import { auth, storage } from '@lib/firebase';
-import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
-import Loader from './Loader';
-import toast from 'react-hot-toast';
+import { useRef, useState } from "react";
+import { auth, storage } from "@lib/firebase";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import Loader from "./Loader";
+import { Button, Code } from "@nextui-org/react";
+import toast from "react-hot-toast";
 
 // Uploads images to Firebase Storage
 export default function ImageUploader() {
@@ -10,54 +11,78 @@ export default function ImageUploader() {
   const [progress, setProgress] = useState(0);
   const [downloadURL, setDownloadURL] = useState(null);
 
+  // File upload hack
+  const fileInputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
   // Creates a Firebase Upload Task
   const uploadFile = async (e) => {
     // Get the file
     const file = Array.from(e.target.files)[0];
-    const extension = file.type.split('/')[1];
+    const extension = file.type.split("/")[1];
 
     // Makes reference to the storage bucket location
-    const fileRef = ref(storage, `uploads/pictures/${auth.currentUser.uid}/${Date.now()}.${extension}`);
+    const fileRef = ref(
+      storage,
+      `uploads/pictures/${auth.currentUser.uid}/${Date.now()}.${extension}`
+    );
     setUploading(true);
 
     // Starts the upload
     const task = uploadBytesResumable(fileRef, file);
 
     // Listen to updates to upload task
-    task.on("state_changed", (snapshot) => {
-      const pct = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0);
-      setProgress(Number(pct));
-    },
-    // Handle errors
-    (error) => {
-      toast.error("An error occurred while uploading the image");
-      setUploading(false);
-    },
-    // Handle successful upload
-    () => {
-      getDownloadURL( task.snapshot.ref ).then((url) => {
-        setDownloadURL(url);
+    task.on(
+      "state_changed",
+      (snapshot) => {
+        const pct = (
+          (snapshot.bytesTransferred / snapshot.totalBytes) *
+          100
+        ).toFixed(0);
+        setProgress(Number(pct));
+      },
+      // Handle errors
+      (error) => {
+        toast.error("An error occurred while uploading the image");
         setUploading(false);
-      });
-    }
-  );
-};
+      },
+      // Handle successful upload
+      () => {
+        getDownloadURL(task.snapshot.ref).then((url) => {
+          setDownloadURL(url);
+          setUploading(false);
+        });
+      }
+    );
+  };
 
   return (
-    <div className="box">
+    <div className="flex justify-between">
       <Loader show={uploading} />
-      {uploading && <h3>{progress}%</h3>}
+      {uploading && <p className="text-lg">{progress}%</p>}
 
       {!uploading && (
         <>
-          <label className="btn">
+          <Button className="m-1" onClick={handleButtonClick}>
             📸 Upload Img
-            <input type="file" onChange={uploadFile} accept="image/x-png,image/gif,image/jpeg" />
-          </label>
+          </Button>
+
+          <input
+            type="file"
+            ref={fileInputRef}
+            onChange={uploadFile}
+            accept="image/x-png,image/gif,image/jpeg"
+            className="hidden"
+          />
         </>
       )}
 
-      {downloadURL && <code className="upload-snippet">{`![alt](${downloadURL})`}</code>}
+      {downloadURL && (
+        <Code className="upload-snippet">{`![alt](${downloadURL})`}</Code>
+      )}
     </div>
   );
 }
