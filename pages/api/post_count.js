9e48185554a45bd1beec63dd6a4a16e2560165b1
unquieterpig <unquieterpig@gmail.com>
2023-11-29 00:16:06 -0500

    Refactor API requests to use collectionGroup and collection functions from Firebase SDK

--- pages/api/post_count.js
+++ pages/api/post_count.js
@@ -1,12 +1,14 @@
 // API request that returns a JSON response with the amount of posts in the Firebase database
-import { firestore } from '../../lib/firebase';
+import { firestore } from "@lib/firebase";
+import { collectionGroup, getDocs } from "firebase/firestore";
 
 export default async (req, res) => {
-    const snapshot = await firestore.collectionGroup('posts').get();
+  // Get data from Firestore
+  const postRef = collectionGroup(firestore, "posts");
+  const snapshot = await getDocs(postRef);
 
-    // Use the size property of the snapshot to get the number of posts
-    const numberOfPosts = snapshot.size;
+  const numberOfPosts = snapshot.size;
 
-    // Return the count as JSON
-    res.status(200).json({ numberOfPosts });
-}
+  // Return the count as JSON
+  res.status(200).json({ numberOfPosts });
+};
--- pages/api/user_count.js
+++ pages/api/user_count.js
@@ -1,12 +1,14 @@
 // API request that returns a JSON response with the amount of registered users in the Firebase database
-import { firestore } from '../../lib/firebase';
+import { firestore } from "@lib/firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 export default async (req, res) => {
-    const snapshot = await firestore.collection('users').get();
+  // Get data from Firestore
+  const userRef = collection(firestore, "users");
+  const snapshot = await getDocs(userRef);
 
-    // Use the size property of the snapshot to get the number of users
-    const numberOfUsers = snapshot.size;
+  const numberOfUsers = snapshot.size;
 
-    // Return the count as JSON
-    res.status(200).json({ numberOfUsers });
-}
+  // Return the count as JSON
+  res.status(200).json({ numberOfUsers });
+};
