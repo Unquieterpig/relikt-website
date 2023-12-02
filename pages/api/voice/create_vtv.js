58b244b268c88bdd3a09da5f0d4b5c2d3579e9a0
Daniel Stiller <des5771@psu.edu>
2023-12-02 02:45:42 -0500

    Added skeleton code

--- pages/api/voice/create_vtv.js
+++ pages/api/voice/create_vtv.js
@@ -2,4 +2,52 @@
 // Headers:
 // Authorization: Bearer <token>
 // Body:
-//
+
+// Need to figure out what is required for POST method
+// VTSUploader.js 
+
+import axios from "axios";
+import fs from "fs";
+import path from "path";
+import { nanoid } from "nanoid";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    console.log("POST request received for /api/voice/create_vtv");
+    try {
+      const {  } = req.body; // TODO
+
+      // TODO: API key and URL
+      //const apiKey = ;
+      const apiUrl = `https://arpeggi.io/api/kits/v1/voice-conversions`;
+
+      // TODO: Default voice settings
+      const defaultVoiceSettings = {
+
+      };
+
+      // TODO: Override default settings with provided values, if any
+      const finalVoiceSettings = { ...defaultVoiceSettings, ...voiceSettings };
+
+      const response = await axios.post(
+        
+      );
+
+      // Generate a random file name
+      const filename = `${nanoid()}.mp3`;
+      const filePath = path.resolve("./public/audio", filename);
+
+      // Save the audio data as an mp3 file
+      fs.writeFileSync(filePath, Buffer.from(response.data, "binary"));
+
+      res
+        .status(200)
+        .json({ audioUrl: `/audio/${filename}`, selectedVoice: voiceId });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
