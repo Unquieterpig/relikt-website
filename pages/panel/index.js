3edd7dfe8abc00d31e578c2fce7240bd40c1d870
unquieterpig <unquieterpig@gmail.com>
2023-12-04 04:11:40 -0500

    Refactor table cell rendering and add custom render for audio sample cells

--- pages/panel/index.js
+++ pages/panel/index.js
@@ -26,7 +26,7 @@ import {
   getKeyValue,
 } from "@nextui-org/react";
 
-import { useState, useMemo } from "react";
+import React, { useState } from "react";
 
 export default function Generate() {
   return (
@@ -93,6 +93,81 @@ function PanelContent() {
       },
     ]);
   };
+
+  // Custom render for table cells
+  const renderCell = React.useCallback((user, columnKey) => {
+    const cellValue = user[columnKey];
+
+    if (columnKey === "audioSample") {
+      // Construct the full URL for the audio file
+      const audioFileUrl = `${window.location.origin}/${cellValue}`;
+
+      return (
+        <Button isIconOnly variant="shadow" aria-label="Download">
+          <a href={audioFileUrl} download>
+            📩
+          </a>
+        </Button>
+      );
+    } else {
+      return cellValue;
+    }
+
+    switch (columnKey) {
+      case "name":
+        return (
+          <User
+            avatarProps={{ radius: "lg", src: user.avatar }}
+            description={user.email}
+            name={cellValue}
+          >
+            {user.email}
+          </User>
+        );
+      case "role":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-sm capitalize">{cellValue}</p>
+            <p className="text-bold text-sm capitalize text-default-400">
+              {user.team}
+            </p>
+          </div>
+        );
+      case "status":
+        return (
+          <Chip
+            className="capitalize"
+            color={statusColorMap[user.status]}
+            size="sm"
+            variant="flat"
+          >
+            {cellValue}
+          </Chip>
+        );
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <Tooltip content="Details">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EyeIcon />
+              </span>
+            </Tooltip>
+            <Tooltip content="Edit user">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EditIcon />
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                <DeleteIcon />
+              </span>
+            </Tooltip>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
 
   return (
     <>
@@ -170,7 +245,7 @@ function PanelContent() {
             {(item) => (
               <TableRow key={item.key}>
                 {(columnKey) => (
-                  <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+                  <TableCell>{renderCell(item, columnKey)}</TableCell>
                 )}
               </TableRow>
             )}
@@ -224,26 +299,3 @@ function PopUpContainer(props) {
     </>
   );
 }
-
-// Unsure if this will be used or the create_tts and create_vtv will
-// function postData() {
-//   if (selectedFile && selectedFileData && selectedVoice) {
-//     // Edit serverURL to correct location
-//     let serverURL = "http://localhost:3001";
-
-//     axios({
-//       method: "post",
-//       url: serverURL,
-//       data: {
-//         // Edit data to correct format
-//         voice: selectedValue,
-//         name: selectedFile.name,
-//         audio: selectedFileData,
-//       },
-//     }).then((response) => {
-//       // Edit response if needed then send to createVoiceCell
-//       // createVoiceCell(response);
-//       console.log(response);
-//     });
-//   }
-// }
